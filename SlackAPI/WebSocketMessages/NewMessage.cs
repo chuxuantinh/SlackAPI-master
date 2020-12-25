5bc8ad1197c5f96f6e936659457b8c6d1569f683
Chu Xuân Tình <50776038+chuxuantinh@users.noreply.github.com>
2020-12-26 02:46:03 +0700

    Initial commit

---
+++ SlackAPI/WebSocketMessages/NewMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SlackAPI.WebSocketMessages
+{
+    [SlackSocketRouting("message")]
+    [SlackSocketRouting("message", "bot_message")]
+    public class NewMessage : SlackSocketMessage
+    {
+        public string user;
+        public string channel;
+        public string text;
+        public string team;
+        public DateTime ts;
+        public DateTime thread_ts;
+
+        public NewMessage()
+        {
+            type = "message";
+        }
+    }
+}
